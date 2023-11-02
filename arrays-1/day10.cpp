5be75b3fac5dbd934f4b578c20d17674274fc41b
Ritwija <ritwija05@gmail.com>
2023-11-02 23:11:09 +0530

    d

---
+++ arrays-1/day10.cpp
@@ -0,0 +1,8 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+     int i = m - 1,j = n - 1,k = m + n - 1;
+         while (j >= 0) 
+			nums1[k--] = i >= 0 && nums1[i] > nums2[j]? nums1[i--]: nums2[j--];
+    }
